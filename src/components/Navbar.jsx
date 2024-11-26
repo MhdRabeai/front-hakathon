e64dbdd6d5aa6ab0c105da4501cd2e703314f1cb
aya2-ze <ayazeno@gmail.com>
2024-11-26 21:29:07 +0300

    ....

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -172,26 +172,24 @@ const Navbar = () => {
               )}
             </button> */}
 
-            {user && (
-              <>
-                <NavLink
-                  className={
-                    "active font-medium text-white focus:outline-none  hover:text-white  focus:text-white transition"
-                  }
-                  to="/dashboard"
-                >
-                  Dashboard
-                </NavLink>
-                <button
-                  className="font-medium text-white focus:outline-none"
-                  onClick={handleLogout}
-                >
-                  <IoMdLogOut />
-                </button>
-              </>
-            )}
-
-            <div className="flex">
+            <NavLink
+              className={
+                "active font-medium text-white focus:outline-none  hover:text-white  focus:text-white transition"
+              }
+              to="/dashboard"
+            >
+              Dashboard
+            </NavLink>
+{/* <Link
+                className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-[#0B0B43] text-white 
+                hover:bg-white hover:text-[#7D7CEC] hover:border-[#7D7CEC] 
+                transition-transform duration-300 ease-in-out 
+                transform hover:scale-105 hover:shadow-lg 
+                focus:outline-none"
+              >
+                Get Started
+              </Link> */}
+            <div className="flex  ">
               <Link
                 to={"/register"}
                 className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-bold rounded-lg border border-transparent bg-[#7D7CEC] text-[#E2E2EB] hover:bg-[#E2E2EB] hover:text-[#7D7CEC] ml-6 transition-transform duration-300 ease-in-out 
