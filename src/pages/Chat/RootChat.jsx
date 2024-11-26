083e2e71fb03bfd61b009cb435d0f84e5ce369a1
MhdRabeai <mhd.rabea.naser@gmail.com>
2024-11-26 17:58:11 +0300

    up

--- src/pages/Chat/RootChat.jsx
+++ src/pages/Chat/RootChat.jsx
@@ -1,8 +1,21 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import LoginChat from "./LoginChat";
+import Navbar from "../../components/Navbar";
+import { Outlet } from "react-router-dom";
+import Footer from "../../components/Footer";
+// import { Outlet } from "react-router-dom";
 
 const RootChat = () => {
-  return <div>RootChat</div>;
+  return (
+    <div className=" min-h-screen bg-[#F6F6F6]  flex flex-col justify-between font-sans">
+      <Navbar />
+      <div className=" relative ">
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default RootChat;
