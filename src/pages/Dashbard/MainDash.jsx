cd914c98661a2a037aa76080e57d2b9245f8992e
MhdRabeai <mhd.rabea.naser@gmail.com>
2024-11-27 05:06:10 +0300

    up

--- src/pages/Dashbard/MainDash.jsx
+++ src/pages/Dashbard/MainDash.jsx
@@ -14,38 +14,6 @@ import BarChart from "../../components/Dashboard/BarChart";
 import StatBox from "../../components/Dashboard/StartBox";
 import ProgressCircle from "../../components/Dashboard/ProgressCirde";
 
-// const fetchUsersData = async () => {
-//   try {
-//     const response = await fetch("/mockData.json");
-//     const data = await response.json();
-//     if (data && data.users) {
-//       return data.users.map((item) => ({
-//         id: item._id,
-//         name: item.fullName,
-//         phone: item.phoneNumber,
-//         location: item.location,
-//         active: item.active ? "Active" : "Inactive",
-//         accessLevel:
-//           item.roleID === 1
-//             ? "admin"
-//             : item.roleID === 2
-//               ? "user"
-//               : item.roleID === 3
-//                 ? "pharmacy"
-//                 : item.roleID === 4
-//                   ? "warehouse"
-//                   : "driver",
-//       }));
-//     } else {
-//       console.error("Unexpected response format:", data);
-//       return [];
-//     }
-//   } catch (error) {
-//     console.error("Error fetching team data:", error);
-//     return [];
-//   }
-// };
-
 const MainDash = () => {
   const [dashboardData, setDashboardData] = useState({
     stats: {
