bb06efe69b0f3004e52717038771861b958c08a1
MhdRabeai <mhd.rabea.naser@gmail.com>
2024-11-27 04:14:08 +0300

    up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-} 
-
-=======
 /* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -18,10 +5,9 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        barcon: ['Bar Con', 'sans-serif'], // Add custom font here
+        barcon: ["Bar Con", "sans-serif"],
       },
     },
   },
   plugins: [require("@tailwindcss/forms"), require("preline/plugin")],
 };
->>>>>>> kamel_old
